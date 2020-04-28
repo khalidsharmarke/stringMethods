d487fd9e8cc7a33380a0d0eba179c80e7c3009ac
Khalid Sharmarke <Khalid.Sharmarke@fmc-na.com>
2020-04-28 13:02:20 -0400

    done

---
+++ i did it.js
@@ -0,0 +1,93 @@
+// 1.
+let coffee = “French”;
+
+let flavor = “Vanilla”;
+
+let order = coffee.concat(flavor)
+
+console.log(order);
+
+// 2. 
+// I have 3 coffees.
+
+// I also have 10 chais.
+
+// I have  a total of 13 drinks total.
+
+
+let coffee = 3;
+
+let chai = 10;
+
+console.log(`I have ${coffee} coffees`);
+
+console.log(`I also have ${chai} chais`);
+
+console.log(`I have a total of ${coffee + chai} drinks total`);
+
+// 3.
+
+function myFunction() {
+
+  let str = "Can you guess what is the index of 42 is? I am sure you will be surprised!";
+
+  let answer = 
+
+console.log(answer);
+
+};
+
+
+// length method: Find how long the string is!
+function myFunction() {
+
+  let str = "Can you guess how long this string is? I am sure you will be surprised! (Hint:It is not 42.)";
+
+  let answer = 92;
+
+console.log(answer);
+
+};
+
+// 4. 
+function myFunction() {
+
+  let str1 = "How you doin?";
+
+  let str2 = str1.replace("HOW", "What");
+
+console.log(str2);
+
+};
+
+// 5.
+function myFunction() {
+
+  var str = "         SOCIAL DISTANCING!!!                    ";
+
+  alert(str.trim());
+
+};
+
+// 6.
+// to uppercase
+function myFunction() {
+
+  let str = "social distancing!!";
+
+  let res = str.toUpperCase();
+
+  console.log(res);
+
+};
+
+//to lowercase
+function myFunction() {
+
+  let str = "QuiEt VoICeS...PLEASE";
+
+  let res = str.toLowerCase();
+
+  console.log(res);
+
+}
